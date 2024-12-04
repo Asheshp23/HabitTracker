6cac3600012983cee9990082efb3b80374341869
asheshp23 <asheshp23@gmail.com>
2024-12-04 10:10:15 -0500

    initial commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Ashesh Patel on 2024-12-04.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
